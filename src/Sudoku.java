0caf30ded9c62bc9b3df2e4d84f9a43c25c13250
nekoFra <francesco.mariani.90@gmail.com>
2016-12-19 15:25:02 +0100

    Sviluppo classi Matrix, Cell con metodi di ricerca

--- src/Sudoku.java
+++ src/Sudoku.java
@@ -5,11 +5,7 @@ public class Sudoku {
 	public static void main(String[] args) {
 			
 		Matrix matrix = new Matrix("C:/Users/Francesco/Google Drive/Documenti/Università/Programmazione di sistemi multicore/Esercitazioni/Sudoku/stage.txt");
-		matrix.showMetrix();
-		
-		System.out.println("");
-		
-		matrix.seekOnStage(4, 4).forEach((a)-> System.out.print(a + " "));
+
 
 	}
 
